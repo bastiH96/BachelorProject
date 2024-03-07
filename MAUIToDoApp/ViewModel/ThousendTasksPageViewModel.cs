57cb18a379c91412d98f8285f8a8ba13600384e7
Sebastian H <sebastianheyde@t-online.de>
2024-03-07 09:58:01 +0100

    MAUI & WPF adjustments on 1000 tasks function

--- MAUIToDoApp/ViewModel/ThousendTasksPageViewModel.cs
+++ MAUIToDoApp/ViewModel/ThousendTasksPageViewModel.cs
@@ -1,6 +1,7 @@
 namespace MAUIToDoApp.ViewModel;
 
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using WpfMauiLibrary.HelperClasses;
@@ -10,29 +11,54 @@ using WpfMauiLibrary.Services.DataAccess;
 public partial class ThousendTasksPageViewModel : ObservableObject
 {
     [ObservableProperty] private ObservableCollection<ToDoTask> openTasks = [];
-    [ObservableProperty] private ObservableCollection<ToDoTask> closedTasks = [];
+    [ObservableProperty] private string elapsedTime;
     private readonly ToDoTaskDbAccess dbAccess = new(Constants.DbMaui1000TasksFullPath);
 
     [RelayCommand]
-    public void CreateThousendTasks()
+    public void CreateThousendTasksForDb()
     {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
         for (var i = 0; i < 1000; i++)
         {
             var task = new ToDoTask($"Task number {i}");
             this.dbAccess.InsertOne(task);
         }
+        stopwatch.Stop();
+        this.ElapsedTime = stopwatch.ElapsedMilliseconds.ToString();
+        stopwatch.Reset();
     }
 
     [RelayCommand]
-    public void LoadThousendTasks() => this.OpenTasks = new(this.dbAccess.GetAll());
+    public void LoadThousendTasksFromDb()
+    {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        this.OpenTasks = new(this.dbAccess.GetAll());
+
+        stopwatch.Stop();
+        this.ElapsedTime = stopwatch.ElapsedMilliseconds.ToString();
+        stopwatch.Reset();
+    }
 
     [RelayCommand]
-    public void DeleteThousendTasks()
+    public void CreateThousendTasks()
     {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
         for (var i = 0; i < 1000; i++)
         {
-            this.dbAccess.DeleteOne(i + 1);
-            this.OpenTasks.RemoveAt(i);
+            var task = new ToDoTask($"Task number {i}");
+            this.OpenTasks.Add(task);
         }
+
+        stopwatch.Stop();
+        this.ElapsedTime = stopwatch.ElapsedMilliseconds.ToString();
+        stopwatch.Reset();
     }
+
+    [RelayCommand]
+    public void EmptyList() => this.OpenTasks.Clear();
 }
